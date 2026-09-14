2e0dbdf05c9e2e995c5958ddbe93c2f772a89a37
criscr777 <aannaluizacarvalho09@gmail.com>
2026-09-13 22:58:28 -0300

    Show video section in birthday album

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../albumromanticovertical";
+import VideoSection from "./VideoSection";
 import "./styles.css";
+
+function Root() {
+  return (
+    <>
+      <App />
+      <VideoSection />
+    </>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 );
