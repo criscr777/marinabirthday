5d6eba56dadd278c97bbf7594de27bdbcd0acdd6
criscr777 <aannaluizacarvalho09@gmail.com>
2026-09-13 23:24:28 -0300

    remove duplicate video page

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../albumromanticovertical";
-import VideoSection from "./VideoSection";
 import "./styles.css";
-
-function Root() {
-  return (
-    <>
-      <App />
-      <VideoSection />
-    </>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>,
 );
